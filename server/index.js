a4a5a9cd698c7d0a73d972840683ce327154dcee
Sam Bisignano <Bisignanosam@gmail.com>
2024-05-23 18:31:50 -0700

    App Running Before Heroku

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 const routes = require('./routes/habitroutes');
@@ -11,8 +12,10 @@ connectDB();
 
 app.use(express.json());
 
-
 app.use('/', routes);
+
+app.use(express.static(path.join(__dirname, '../client/hb/build')));
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
